00f4fa29fe2241f4a3513092c9b0df43c15f2550
Scott Pakin <scott+gpg@pakin.org>
2021-01-17 15:45:53 -0700

    Write some brief package documentation

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package xmorph provides functions for warping and morping images by deforming
+one given mesh into another.  It is primarily a Go wrapper for the venerable
+libmorph library (http://xmorph.sourceforge.net/), which underlies the morph
+CLI and the xmorph and gtkmorph GUIs.
+*/
+package xmorph
